5347492708a96e6ede3c65421093b693b9b79e55
Shashank Bhalotia <sbhalotia9@gmail.com>
2017-06-05 14:34:34 +0000

    Adding GENSIN_f() function and changing ScilabDouble for proper submit

--- webapp/combined.js
+++ webapp/combined.js
@@ -6916,31 +6916,49 @@ function GENSIN_f() {
     }
 }
 
-function GENSQR_f() {
+function GENSQR_f () {
 
-    GENSQR_f.prototype.define = function GENSQR_f() {
-        this.Amplitude = 1;
+GENSQR_f.prototype.define = function GENSQR_f() {
+    this.Amplitude = 1;
+    var model = scicos_model()
+    model.sim = new ScilabString(["gensqr"]);
+    model.out = new ScilabDouble([1]);
+    model.out2 = new ScilabDouble([1]);
+    model.outtyp = new ScilabDouble([1]);
+    model.evtin = new ScilabDouble([1]);
+    model.dstate = new ScilabDouble([this.Amplitude]);
+    model.blocktype = new ScilabString(["d"]);
+    model.dep_ut = new ScilabBoolean([false, false]);
+    var exprs = new ScilabString([this.Amplitude]);
+    var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"GENSQR_f\",sz(1),sz(2));"]);
+    this.x = new standard_define(new ScilabDouble([3, 2]), model, exprs, gr_i);
+    return new BasicBlock(this.x);
 
-        var model = scicos_model();
-        model.sim = new ScilabString(["gensqr"]);
-        model.out = new ScilabDouble([1]);
-        model.out2 = new ScilabDouble([1]);
-        model.outtyp = new ScilabDouble([1]);
-        model.evtin = new ScilabDouble([1]);
-        model.dstate = new ScilabDouble([this.Amplitude]);
-        model.blocktype = new ScilabString(["d"]);
-        model.dep_ut = new ScilabBoolean([false, false]);
 
-        var exprs = new ScilabString([this.Amplitude]);
+    }
+GENSQR_f.prototype.get = function GENSQR_f() {
 
-        var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"GENSQR_f\",sz(1),sz(2));"]);
-        this.x = new standard_define(new ScilabDouble([3, 2]), model, exprs, gr_i);
-        return new BasicBlock(this.x);
+    var options={
+        Amplitude:["Amplitude",this.Amplitude.toString().replace(/,/g, " ")],
+    };
+    return options;
+    }
+
+GENSQR_f.prototype.set = function GENSQR_f() {
+
+    this.Amplitude = parseInt(arguments[0]["Amplitude"]);
+    var exprs = new ScilabString([this.Amplitude]);
+    this.x.model.out2=1;
+    this.x.model.dstate=new ScilabDouble([this.Amplitude]);
+    this.x.model.outtyp=1;
+    this.x.graphics.exprs = exprs;
+    return new BasicBlock(this.x);  
     }
     GENSQR_f.prototype.details = function GENSQR_f() {
         return this.x;
     }
 }
+
 function GOTO() {
 
     GOTO.prototype.define = function GOTO() {
