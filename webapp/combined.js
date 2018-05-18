60303c29d38a1a5b931fc80dbc85bcf57f4e0aeb
eaglewarrior <rinkinag24@gmail.com>
2018-05-18 16:17:39 +0530

    function for clr block

--- webapp/combined.js
+++ webapp/combined.js
@@ -22264,7 +22264,7 @@ function TIME_f() {
         model.dep_ut = new ScilabBoolean([false, true]);
 
         var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"TIME_f\",sz(1),sz(2));"]);
-        this.x = new standard_define(new ScilabDouble([2, 2]), model, new ScilabDouble(), gr_i);
+        this.x = new standard_define(new ScilabDouble([2, 2]), model, [], gr_i);
         return new BasicBlock(this.x);
     }
 
