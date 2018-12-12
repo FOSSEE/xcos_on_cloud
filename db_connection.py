26780b40b15a1a0e9af6a742273a3f358bd08375
Sunil Shetye <sunilshetye@rocketmail.com>
2018-12-12 16:59:15 +0530

    move queries to the config file

--- db_connection.py
+++ db_connection.py
@@ -3,10 +3,8 @@ from config import DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT
 
 def connection():
     conn = MySQLdb.connect(host=DB_HOST,
-                           user = DB_USER,
-                           passwd =  DB_PASS,
-                           db = DB_NAME,
-                           port = DB_PORT)
-    c = conn.cursor()
-
-    return c, conn
+                           user=DB_USER,
+                           passwd=DB_PASS,
+                           db=DB_NAME,
+                           port=DB_PORT)
+    return conn.cursor()
