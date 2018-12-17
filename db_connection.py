964eaf624932d1692697c95a4046a1a51f1f04e9
prashantsinalkar <prashantsinalkar@gmail.com>
2018-12-17 13:13:35 +0530

    formated the python code

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,7 @@
 import MySQLdb
 from config import DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT
 
+
 def connection():
     conn = MySQLdb.connect(host=DB_HOST,
                            user=DB_USER,
