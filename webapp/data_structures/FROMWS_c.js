46684827269ce257b14da543f9d632f6850809d5
dipti <diptiiitb17@gmail.com>
2019-03-13 18:04:06 +0530

    Fixed ipar issue and small other fix

--- webapp/data_structures/FROMWS_c.js
+++ webapp/data_structures/FROMWS_c.js
@@ -5,13 +5,14 @@ function FROMWS_c () {
         this.Method = 1;
         this.ZC = 1;
         this.OutEnd = 0;
+        this.str2code = -31; //this is value which we get from _str2code(varnam) function.
 
         var model = scicos_model();
         model.sim = list(new ScilabString(["fromws_c"]), new ScilabDouble([4]));
         model.out = new ScilabDouble([-1]);
         model.out2 = new ScilabDouble([-2]);
         model.outtyp = new ScilabDouble([-1]);
-        model.ipar = new ScilabDouble([this.varnam.length],[get_str2code],[this.Method],[this.ZC],[this.OutEnd])
+        model.ipar = new ScilabDouble([this.varnam.length],[this.str2code],[this.Method],[this.ZC],[this.OutEnd])
         model.evtin =  new ScilabDouble([1]);
         model.evtout = new ScilabDouble([1]);
         model.firing = new ScilabDouble([0]);
