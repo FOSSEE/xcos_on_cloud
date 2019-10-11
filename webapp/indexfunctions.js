1b7645df8096c8a17be4e7bbf49ecc9824421328
Sunil Shetye <sunilshetye@rocketmail.com>
2019-10-11 12:37:49 +0530

    use same function for adding a new cell

--- webapp/indexfunctions.js
+++ webapp/indexfunctions.js
@@ -3646,64 +3646,12 @@ function addSidebarIcon(graph, sidebar, name, image, dimensions) {
     var funct = function(graph, evt, cell, x, y) {
         var parent = graph.getDefaultParent();
         var model = graph.getModel();
-        var v1 = null;
         var doc = mxUtils.createXmlDocument();
         model.beginUpdate();
         try {
             var details_instance = new window[name]();
             var details = details_instance.define();
-            var enc = new mxCodec(mxUtils.createXmlDocument());
-            var node = enc.encode(details);
-            var temp = enc.encode(parent);
 
-            // Get the stylesheet for the graph
-            var stylesheet = graph.getStylesheet();
-            // From the stylesheet, get the style of the particular block
-            var style = stylesheet.styles[name];
-            // To get dimension(height,width) of a block
-            var dimensionForBlock = details_instance.getDimensionForDisplay();
-            height = dimensionForBlock["height"]; //returns height of block
-            width = dimensionForBlock["width"]; //returns width of block
-
-            /*
-             * When a particular block is loaded for the first time, the image
-             * in the style of the block will be a path to the image. Set the
-             * label in the style property of the block has a html image, and
-             * set the image in the style property as null
-             *
-             * NOTE: Since the image of any block need not be changed for every
-             * movement of that block, the image must be set only once.
-             */
-            if (style != null && style['image'] != null) {
-                // Make label as a image html element
-                var label = '<img src="' + style['image'] + '" height="' + (height*0.9) + '" width="' + (width*0.9) + '">';
-
-                // Set label
-                style['label'] = label;
-
-                style['imagePath'] = style['image'];
-
-                // Set image as null
-                style['image'] = null;
-
-                // Add the label as a part of node
-                node.setAttribute('label', label);
-            }
-
-            /*
-             * If a particular block with image tag in its style property has
-             * been invoked already, the image tag would be null for any
-             * successive instances of the same block. Hence, set the label
-             * from the label tag in style which was set when that blockModel
-             * was invoked on the first time.
-             */
-            if (style != null && style['label'] != null) {
-                // Set label from the label field in the style property
-                node.setAttribute('label', style['label']);
-            }
-
-            node.setAttribute('parent', temp.getAttribute('id'));
-            var i, arr = [];
             var blockModel = details_instance.x.model;
             var graphics = details_instance.x.graphics;
 
@@ -3713,36 +3661,36 @@ function addSidebarIcon(graph, sidebar, name, image, dimensions) {
             var controlPorts = [];
             var commandPorts = [];
             if (blockModel.in.height != null) {
-                arr = getData(graphics.in_implicit);
+                var arr = getData(graphics.in_implicit);
                 if (arr.length != 0) {
                     inputPorts = arr;
                 } else {
-                    for (i = 0; i < blockModel.in.height; i++) {
+                    for (var i = 0; i < blockModel.in.height; i++) {
                         inputPorts.push("E");
                     }
                 }
             }
             if (blockModel.out.height != null) {
-                arr = getData(graphics.out_implicit);
+                var arr = getData(graphics.out_implicit);
                 if (arr.length != 0) {
                     outputPorts = arr;
                 } else {
-                    for (i = 0; i < blockModel.out.height; i++) {
+                    for (var i = 0; i < blockModel.out.height; i++) {
                         outputPorts.push("E");
                     }
                 }
             }
             if (blockModel.evtin.height != null) {
-                for (i = 0; i < blockModel.evtin.height; i++) {
+                for (var i = 0; i < blockModel.evtin.height; i++) {
                     controlPorts.push("CONTROL");
                 }
             }
             if (blockModel.evtout.height != null) {
-                for (i = 0; i < blockModel.evtout.height; i++) {
+                for (var i = 0; i < blockModel.evtout.height; i++) {
                     commandPorts.push("COMMAND");
                 }
             }
-            v1 = graph.insertVertex(parent, null, node, x, y, width, height, name);
+            var v1 = updateDetails(graph, cell, details, true);
 
             // @Chhavi: Additional attribute to store the block's instance
             v1.blockInstance = createInstanceTag(details_instance);
@@ -3751,10 +3699,10 @@ function addSidebarIcon(graph, sidebar, name, image, dimensions) {
             v1.flipY = 1;
             createPorts(graph, v1, inputPorts, controlPorts, outputPorts, commandPorts, null, null, details_instance.x.model);
             v1.setConnectable(false);
+            graph.setSelectionCell(v1);
         } finally {
             model.endUpdate();
         }
-        graph.setSelectionCell(v1);
     }
 
     var blockFigure = document.createElement('td');
