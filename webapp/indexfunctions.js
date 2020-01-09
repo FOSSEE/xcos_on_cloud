05697d874bfdbef097a73a4cfa21fd789c839854
dipti <diptiiitb17@gmail.com>
2020-01-09 16:48:31 +0530

    Added condition as per review

--- webapp/indexfunctions.js
+++ webapp/indexfunctions.js
@@ -3700,7 +3700,9 @@ function addSidebarIcon(graph, sidebar, name, image, dimensions) {
             }
             var geometryCell = new mxGeometry(x, y, 0, 0);
             var v1 = updateDetails(graph, null, details, details_instance, name, geometryCell, true);
-            details_instance.setID(v1.id);
+            if(name == "CMATVIEW"){
+                details_instance.setID(v1.id);
+            }
             // @Chhavi: Additional attribute to store the block's instance
             v1.blockInstance = createInstanceTag(details_instance);
             v1.currentAngle = 0;
