1df80883d19e0bdeb6544fc58ba1957eb87aa5a1
dipti <diptiiitb17@gmail.com>
2020-05-18 18:18:49 +0530

    Fixed 4 block JS, modified func and fix import for some block

--- webapp/data_structures/GAINBLK.js
+++ webapp/data_structures/GAINBLK.js
@@ -28,100 +28,52 @@ function GAINBLK() {
         return this.x;
     }
     GAINBLK.prototype.get = function GAINBLK() {
-        if(this.over == undefined || this.over == null)
-            var options = "";
+        if(this.over == undefined || this.over == null){
             this.over = 0;
-        var str = this.gain.toString();
-        if (!str.match(/[a-z()+\-*/.^{}]/i)) {
-		    if(str.match(/\[[0-9]+\]/)){
-		    	options = {
-                    gain:["Gain", this.gain],
-            	    over:["Do On Overflow(0=Nothing 1=Saturate 2=Error)",this.over],
-        		};
-		    }else {
-			    options = {
-            		gain:["Gain", sci2exp(this.gain)],
-            		over:["Do On Overflow(0=Nothing 1=Saturate 2=Error)",this.over],
-        		};
-		    }
-
-		}else {
-			options = {
-            	gain:["Gain", this.gain],
-            	over:["Do On Overflow(0=Nothing 1=Saturate 2=Error)",this.over],
-        	};
-		}
+        }
+        var options = {
+            gain:["Gain", this.gain],
+            over:["Do On Overflow(0=Nothing 1=Saturate 2=Error)",this.over],
+        };
         return options
     }
     GAINBLK.prototype.set = function GAINBLK() {
-		var exprs = "";
-		this.over = parseFloat((arguments[0]["over"]));
+
+		this.over = parseFloat(arguments[0]["over"]);
         if((this.over < 0)||(this.over > 2)){
         	alert("Do on Overflow must be 0,1,2");
         	throw "incorrect";
     	}
-		var str = arguments[0]["gain"];
-        if(str.length == 0){
+		var temp_gain = arguments[0]["gain"];
+		var gain_1 = getValueOfImaginaryInput(temp_gain, "GAINBLK");
+	    if (gain_1 == "null") {
+			gain_1 = inverse(temp_gain);
+		}
+        if(gain_1.length == 0){
         	alert("Gain must have at least one element");
         	throw "incorrect";
     	}
-		str = convertInputVectorFormat(str);
-		if (str.match(/[a-z()+\-*/.^{}]/i)) {
-			var value = getValueOfImaginaryInput(str, "GAINBLK");
-			if (value == "null") {
-				throw "incorrect";
-			} else {
-				exprs = new ScilabString([str],[this.over]);
-				this.x.model.rpar = list(new ScilabDouble([value]));
-				this.displayParameter = [arguments[0]["gain"]];
-				this.x.model.intyp = new ScilabDouble([1]);
-        		this.x.model.outtyp = new ScilabDouble([1]);
-        		this.x.model.sim = list(new ScilabString(["gainblk"]), new ScilabDouble([4]));
-        		this.x.model.opar = list();
-				this.out = size(value,1)
-    			this.in = size(value,2)
-    			if(this.out*this.in == 1){
-        			var io = set_io(this.x.model,this.x.graphics,[[-1],[-2]],[[-1],[-2]],[],[])
-    			}else{
-        			var io = set_io(this.x.model,this.x.graphics,[[this.in],[-1]],[[this.out],[-1]],[],[])
-    			}
-				this.gain = str;
-			}
-		} else {
-			var str = arguments[0]["gain"];
-		    str = convertInputVectorFormat(str);
-			this.gain = inverse(str);
-			this.out = size(this.gain,1);
-    		this.in = size(this.gain,2);
-    		if(this.out*this.in == 1){
-        		var io = set_io(this.x.model,this.x.graphics,[[-1],[-2]],[[-1],[-2]],[],[]);
-    		}
-    		else{
-        		var io = set_io(this.x.model,this.x.graphics,[[this.in],[-1]],[[this.out],[-1]],[],[]);
-    		}
-
-			this.x.model.rpar = list(new ScilabDouble([this.gain]));
-			this.x.model.intyp = new ScilabDouble([1]);
-        	this.x.model.outtyp = new ScilabDouble([1]);
-        	this.x.model.sim = list(new ScilabString(["gainblk"]), new ScilabDouble([4]));
-        	this.x.model.opar = list();
-			if(str.match(/\[[0-9]+\]/)){
-				this.displayParameter = [arguments[0]["gain"]];//[sci2exp(this.c)];
-				exprs = new ScilabString(["["+this.gain+"]"],[this.over]);
-				this.gain=["["+this.gain+"]"];
-
-			}else{
-				this.displayParameter = [arguments[0]["gain"]];
-				exprs = new ScilabString([sci2exp(this.gain)],[this.over]);
-			}
-
-		}
-		this.x.graphics.exprs = exprs;
+	    this.x.model.rpar = list(new ScilabDouble([gain_1]));
+		this.x.model.intyp = new ScilabDouble([1]);
+        this.x.model.outtyp = new ScilabDouble([1]);
+        this.x.model.sim = list(new ScilabString(["gainblk"]), new ScilabDouble([4]));
+        this.x.model.opar = list();
+		this.out = size(gain_1,1);
+		this.in = size(gain_1,2);
+        if(this.out*this.in == 1){
+            var io = set_io(this.x.model,this.x.graphics,[[-1],[-2]],[[-1],[-2]],[],[]);
+        }else{
+            var io = set_io(this.x.model,this.x.graphics,[[this.in],[-1]],[[this.out],[-1]],[],[]);
+        }
+        this.gain = temp_gain;
+        this.displayParameter = [this.gain];
+        var exprs = new ScilabString([this.gain],[this.over]);
+        this.x.graphics.exprs = exprs;
 		return new BasicBlock(this.x);
 
     }
     GAINBLK.prototype.get_popup_title = function GAINBLK() {
-        var set_param_popup_title="Set gain block parameters";
+        var set_param_popup_title = "Set gain block parameters";
         return set_param_popup_title
     }
     GAINBLK.prototype.getDimensionForDisplay = function GAINBLK(){
