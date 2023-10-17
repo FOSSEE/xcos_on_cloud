87fc0169af16ae5e9fc7046589eefb8d75b4e7ab
Sunil Shetye <sunilshetye@rocketmail.com>
2023-10-17 23:45:26 +0530

    get container ip address using iproute2

--- run.sh
+++ run.sh
@@ -5,5 +5,10 @@ set -e
 service mysql start
 sleep 2
 
+IPADDR=$( ip addr show | awk -F '[ /]+' '/inet .* eth/ { print $3; }' )
+sed -i \
+    -e "s/\\(HTTP_SERVER_HOST = \\).*/\\1'${IPADDR}'/" \
+    config.py
+
 . env/bin/activate
 python ./SendLog.py
