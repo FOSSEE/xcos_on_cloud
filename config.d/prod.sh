eb090301b7473851410729ba11199ce326c0e3d2
Sunil Shetye <sunilshetye@rocketmail.com>
2024-01-15 13:48:45 +0530

    check if the parameter is a valid ascii string

--- config.d/prod.sh
+++ config.d/prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sed -i \
-    -e 's/\(VERSIONED_CHECK_INTERVAL =\).*/\1 3600/g' \
     -e 's/\(SCILAB_INSTANCE_TIMEOUT_INTERVAL =\).*/\1 250/g' \
+    -e 's/\(SCILAB_MAX_INSTANCES =\).*/\1 150/g' \
     -e 's/\(SCILAB_MIN_INSTANCES =\).*/\1 25/g' \
     -e 's/\(SCILAB_START_INSTANCES =\).*/\1 40/g' \
-    -e 's/\(SCILAB_MAX_INSTANCES =\).*/\1 150/g' \
+    -e 's/\(VERSIONED_CHECK_INTERVAL =\).*/\1 3600/g' \
     config.py
